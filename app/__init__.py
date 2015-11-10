05e0f4d40ce2ef7497b54d0adbcdc438b2c3c3db
Dmitry Ogurtsov <zorbis11@gmail.com>
2015-11-10 10:47:16 +0200

    structure fixed

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['MONGODB_SETTINGS'] = {'HOST': os.environ.get('MONGOLAB_URI', 'mongodb://localhost/demo')}
+app.config['MONGODB_SETTINGS'] = {'host': os.environ.get('MONGOLAB_URI', 'mongodb://localhost/demo')}
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'secret-key')
 
 db = mongoengine.MongoEngine(app)
